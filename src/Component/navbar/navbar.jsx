d60fcc706d72221b77e6f32efafc3b21e6b58eaa
Yuliya110692 <122671093+Yuliya110692@users.noreply.github.com>
2023-10-18 14:56:08 +0100

    add nav and created folder

---
+++ src/Component/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css'; // Import Bootstrap CSS
+import "./navbar.css"
+
+function Navbar() {
+    return (
+       <div className="navbar">
+      <div className="logo">Booking Session</div>
+    </div>
+    )
+}
+
+export default Navbar;
